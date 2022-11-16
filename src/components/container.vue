b40a55f3468967b942e6016a6892d1b9b5201d2e
Vladimir <shadrov_vladimir@mail.ru>
2022-11-16 17:53:43 +0300

    feat: Компонент контейнера #main

---
+++ src/components/container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h2>Заголовок контейнера</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-container',
+};
+</script>
+
+<style scoped>
+.container {
+  border: 1px solid grey;
+}
+</style>
