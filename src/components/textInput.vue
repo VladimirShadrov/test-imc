44868b8f5e7142f3c37247d1b69f2e9aafa7a298
Vladimir <shadrov_vladimir@mail.ru>
2022-11-16 17:51:15 +0300

    feat: Текстовый инпут #main

---
+++ src/components/textInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Заголовок инпута</p>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-text-input',
+};
+</script>
+
+<style scoped></style>
