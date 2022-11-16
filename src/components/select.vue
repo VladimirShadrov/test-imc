bbef09d092747d7b5f939affefa6f53ef2fc4f86
Vladimir <shadrov_vladimir@mail.ru>
2022-11-16 17:57:15 +0300

    feat: Компонет выпадающего списка #main

---
+++ src/components/select.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Заголовок селекта</p>
+    <select>
+      <option disabled value="">Выберите значение</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-select',
+};
+</script>
+
+<style scoped></style>
