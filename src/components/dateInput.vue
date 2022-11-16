498ed238c76d89de7a0957576a3a6fc8939deff2
Vladimir <shadrov_vladimir@mail.ru>
2022-11-16 17:55:19 +0300

    feat: Компонент датапикера #main

---
+++ src/components/dateInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Это заголовок датапикера</p>
+    <input type="date" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-date-input',
+};
+</script>
+
+<style scoped></style>
